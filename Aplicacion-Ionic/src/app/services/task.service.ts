60b437ef6bbcaa1132dafb64703abf53ef72280a
jonathanfdez77 <145861565+jonathanfdez77@users.noreply.github.com>
2024-02-25 15:45:48 -0500

    Components HTML

--- Aplicacion-Ionic/src/app/services/task.service.ts
+++ Aplicacion-Ionic/src/app/services/task.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Task } from '../interfaces/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private api = 'https://jsonplaceholder.typicode.com';
+
+  constructor(private http: HttpClient) { }
+
+  getAllTasks() {
+    const path = `${this.api}/todos/`;
+    return this.http.get<Task[]>(path);
+  }
+
+  getTask(id: string) {
+    const path = `${this.api}/todos/${id}`;
+    return this.http.get<Task>(path);
+  }
 }
+
